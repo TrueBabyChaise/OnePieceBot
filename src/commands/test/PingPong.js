d2b4bcdc9026d248fed178976df2b9edb6baa38d
TrueBabyChaise <tomaugier23@gmail.com>
2021-05-01 01:14:52 +0200

    Add Ping pong cmd

--- src/commands/test/PingPong.js
+++ src/commands/test/PingPong.js
@@ -1,20 +1,20 @@
 const BaseCommand = require('../../utils/structures/BaseCommand');
-const {Client, Message} = require("discord.js");
+const { Client, Message } = require("discord.js");
 
 module.exports = class PingPongCommand extends BaseCommand {
-  constructor() {
-    super('ping', 'testing', ['pg', 'pong', 'Ping', 'Pong']);
-  }
+	constructor() {
+		super('ping', 'testing', ['pg', 'pong', 'Ping', 'Pong']);
+	}
 
-  /**
-   * @param {Client} client
-   * @param {Message} message
-   * @param {Array} args
-   */
-  async run(client, message, args) {
-    if (message.content.slice(client.prefix.length).toLowerCase() == 'ping')
-        message.channel.send('Pong');
-    else 
-        message.channel.send('Ping');
-  }
+	/**
+	 * @param {Client} client
+	 * @param {Message} message
+	 * @param {Array} args
+	 */
+	async run(client, message, args) {
+		if (message.content.slice(client.prefix.length).toLowerCase() == 'ping')
+			message.channel.send('Pong');
+		else
+			message.channel.send('Ping');
+	}
 }
